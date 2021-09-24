9436c631e68fb911c3162c55ab8d6d4b1b25d65a
Subodh Kalika <subodh.kalika@brothersit.com.au>
2021-09-24 10:44:32 +1000

    Fake situation data

---
+++ ui/public-src/src/app/shared/constants/fakeData.js
@@ -0,0 +1,22 @@
+const situations = [
+    {
+        id: "1",
+        name: "Room is hot",
+    },
+    {
+        id: "2",
+        name: "Party going on",
+    },
+    {
+        id: "3",
+        name: "Normal temperature",
+    },
+    {
+        id: "4",
+        name: "Room is cold",
+    }
+]
+
+export default FAKE_DATA = {
+    situations: situations
+}
